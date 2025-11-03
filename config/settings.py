4a8b56e3d90969d9b852bfbc5693d6bfc7b108f6
y-maeda78 <prknowledge.my78@gmail.com>
2025-11-03 16:54:54 +0900

    15th commit 1103

--- config/settings.py
+++ config/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 import environ  #追記
 from django.contrib import messages
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
